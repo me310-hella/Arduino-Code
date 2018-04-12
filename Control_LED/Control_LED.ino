0131457e33afa22d400421c34329456d7cfc6325
Maik Zarnbach <maik.zarnbach@student.hpi.de>
2018-04-12 14:48:41 +0200

    receive bluetooth data and send it back

--- Control_LED/Control_LED.ino
+++ Control_LED/Control_LED.ino
@@ -6,28 +6,30 @@
 * This program lets you to control a LED on pin 13 of arduino using a bluetooth module
 */
 char data = 0;            //Variable for storing received data
+int LED_PIN = 7;
 
 void toggleLED()
 {
-  if(digitalRead(7) == HIGH){
-    digitalWrite(7, LOW);
+  if(digitalRead(LED_PIN) == HIGH){
+    digitalWrite(LED_PIN, LOW);
   }
-  else { digitalWrite(7, HIGH); }
+  else { digitalWrite(LED_PIN, HIGH); }
 }
 
 void setup()
 {
     Serial.begin(9600);   //Sets the baud for serial data transmission                               
-    pinMode(7, OUTPUT);  //Sets digital pin 7 as output pin
+    pinMode(LED_PIN, OUTPUT);  //Sets digital pin 7 as output pin
+
 }
 void loop()
 {
    if(Serial.available() > 0)      // Send data only when you receive data:
    {
       data = Serial.read();        //Read the incoming data & store into data
-      Serial.print(data);          //Print Value inside data in Serial monitor
-      Serial.print("\n");        
-      if(data == '1'){
+      Serial.write("0,0");          //Print Value inside data in Serial monitor
+      Serial.write("\n");        
+      if(strcmp(data, "1")){
         toggleLED();
       }
    }
